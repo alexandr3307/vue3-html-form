c086dfa306efcfd784e09e91705d0d31dca6714c
alexlandar <a.landar@doctis.ru>
2024-03-31 12:55:19 +0300

    remove font family in #app

--- src/App.vue
+++ src/App.vue
@@ -26,11 +26,9 @@
 <style lang="scss">
   @import "assets/css/normalize.css";
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
     margin-top: 60px;
   }
   body {
